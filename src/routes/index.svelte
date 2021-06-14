d3bf8cadaa0134d85260b941ae1471b3db9048a1
v-rogg <mail@valentinrogg.de>
2021-06-14 12:47:25 +0200

    Init new sveltekit project

---
+++ src/routes/index.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+	<title>Obacht! SDK Configurator</title>
+</svelte:head>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
