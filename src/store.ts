ad26f3dc60d4014ca889d0a81f73837aede8e723
v-rogg <mail@valentinrogg.de>
2021-06-18 22:48:50 +0200

    Change websocket store from localhost to local network ip

--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,7 @@
 import { readable } from "svelte/store";
 
 export const socketStore = readable("", set => {
-    const socket = new WebSocket('ws://localhost:3000/ws');
+    const socket = new WebSocket('ws://192.168.178.48:3000/ws');
 
     socket.addEventListener('open', () => console.log('socket opened'));
     socket.addEventListener('message', (event) => set(event.data));
