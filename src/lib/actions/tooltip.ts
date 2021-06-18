86b3e3146c5f4b7b77cb3f85ede47ee48027fe56
v-rogg <mail@valentinrogg.de>
2021-06-18 23:27:22 +0200

    Remove deprecated comments

--- src/lib/actions/tooltip.ts
+++ src/lib/actions/tooltip.ts
@@ -1,7 +1,6 @@
 import Tooltip from '$lib/Tooltip.svelte';
 
 export function tooltip(element) {
-    // let div;
     let title;
     let tooltipComponent;
     function mouseOver(event) {
@@ -44,51 +43,3 @@ export function tooltip(element) {
     }
 }
 
-// export function tooltip(element) {
-//     let div;
-//     let title;
-//     function mouseOver(event) {
-//         console.log("over");
-//         // NOTE: remove the `title` attribute, to prevent showing the default browser tooltip
-//         // remember to set it back on `mouseleave`
-//         title = element.getAttribute('title');
-//         element.removeAttribute('title');
-//
-//         div = document.createElement('div');
-//         div.textContent = title;
-//         div.style = `
-// 			border: 1px solid #ddd;
-// 			box-shadow: 1px 1px 1px #ddd;
-// 			background: white;
-// 			border-radius: 4px;
-// 			padding: 4px;
-// 			position: absolute;
-// 			top: ${event.pageX + 5}px;
-// 			left: ${event.pageY + 5}px;
-// 		`;
-//         document.body.appendChild(div);
-//     }
-//     function mouseMove(event) {
-//         console.log("move");
-//         div.style.left = `${event.pageX + 5}px`;
-//         div.style.top = `${event.pageY + 5}px`;
-//     }
-//     function mouseLeave() {
-//         console.log("leave");
-//         document.body.removeChild(div);
-//         // NOTE: restore the `title` attribute
-//         element.setAttribute('title', title);
-//     }
-//
-//     element.addEventListener('mouseover', mouseOver);
-//     element.addEventListener('mouseleave', mouseLeave);
-//     element.addEventListener('mousemove', mouseMove);
-//
-//     return {
-//         destroy() {
-//             element.removeEventListener('mouseover', mouseOver);
-//             element.removeEventListener('mouseleave', mouseLeave);
-//             element.removeEventListener('mousemove', mouseMove);
-//         }
-//     }
-// }
