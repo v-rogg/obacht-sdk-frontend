a0249e196d86ecfb91460e373ef17677cf8ff12a
v-rogg <mail@valentinrogg.de>
2021-06-21 12:18:54 +0200

    Add hotkeys Add tooltips

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,42 +1,22 @@
 <script lang='ts'>
     import Three from '$lib/Three/index.svelte';
-    import HzDisplay from '$lib/HzDisplay.svelte';
-    import PingDisplay from '$lib/PingDisplay.svelte';
-    import { tooltip } from '$lib/actions/tooltip';
+A    import { tooltip } from '$lib/actions/tooltip';
+    import { hotkey } from '../lib/actions/hotkeys';
+    import { hotkeysStore } from '../store';
 
+    let hotkeysProxy = "";
+
+    hotkeysStore.subscribe(val => {
+        hotkeysProxy = val;
+    })
+
+    function test() {
+        console.log('Hotkey test worked!')
+    }
 </script>
 
 <style lang='sass'>
     @import './src/style/theme'
-
-    .box
-        box-shadow: $shadow-md
-        width: max-content
-        padding: 1rem
-        border-radius: $border-radius
-        position: fixed
-        top: 8rem
-        left: 2rem
-        border: .5px solid $light-grey
-        display: grid
-        gap: .8rem
-        background: $white
-
-        /* slightly transparent fallback */
-        background: rgba($white, .9)
-
-        /* if backdrop support: very transparent and blurred */
-        @supports ((-webkit-backdrop-filter: blur(4px)) or (backdrop-filter: blur(4px)))
-            background: transparentize($white, .8)
-            backdrop-filter: blur(4px)
-
-        .margin-right
-            margin-right: .25rem
-
-        .sensor
-            display: flex
-            align-items: center
-            cursor: default
 
     .compass
         position: fixed
@@ -53,10 +33,6 @@
         position: fixed
         top: 2rem
         left: 2rem
-
-    .mono
-        font-family: "Obacht! Mono", sans-serif
-        margin: 0 .5rem
 
     nav
         position: fixed
@@ -78,23 +54,40 @@
             align-items: center
             border-radius: $border-radius
             border: .5px solid $light-grey
-            transition: 125ms ease
+            //transition: 125ms ease
+
+            /* slightly transparent fallback */
+            background: rgba($white, .9)
+
+            /* if backdrop support: very transparent and blurred */
+            @supports ((-webkit-backdrop-filter: blur(4px)) or (backdrop-filter: blur(4px)))
+                background: rgba($white, .2)
+                backdrop-filter: blur(4px)
 
             &:hover:not(.open)
                 cursor: pointer
                 box-shadow: $shadow-algolia-black
-                border: .5px solid $black
+                //border: .5px solid $black
                 //color: $green
 
+            &:active:not(.open)
+                border: .5px solid $blue
+
         .open
-            border: .5px solid $black
+            border: .5px solid $blue
             box-shadow: $shadow-algolia-black
 
+    h5
+        font-size: $size-small
+        font-weight: 400
+        margin: 0
+        padding: 0
 
 	h1
         font-family: "Obacht! Display", sans-serif
         margin: 0 0 .25rem
         padding: 0
+        font-size: 2rem
 
 </style>
 
@@ -102,28 +95,36 @@
 	<title>Obacht! SDK Configurator</title>
 </svelte:head>
 
+
 <Three/>
 
 <div class='header'>
     <h1>SDK Configurator</h1>
-    <div>Your IP <span class='mono'>-></span> 192.168.178.48</div>
-</div>
-
-<div class='box'>
-    <div><i class="fas fa-sensor-on margin-right"></i>   Connected Sensors</div>
-    <div class='sensor'><span title='Sensor IP' use:tooltip>192.168.178.110</span>   <span class='mono'>-></span>   <span title='Sensor Model' use:tooltip>RPLidar S1</span>   <span class='mono'>-></span><HzDisplay/> <PingDisplay/></div>
+    <h5>v 0.9beta</h5>
 </div>
 
 <nav>
     <div>
-<!--        <i class="fas fa-sensor-on"></i>-->
-        <i class="fas fa-broadcast-tower"></i>
+        <i class="far fa-broadcast-tower event-none"></i>
     </div>
-    <div class='open'>
-        <i class="fas fa-chart-network"></i>
+    <div
+        class='open'
+        title='Show Sensor Raw Data'
+        hotkey={hotkeysProxy.layerRawData}
+        use:hotkey
+        use:tooltip
+        on:click={test}
+    >
+        <i class="far fa-chart-network event-none"></i>
     </div>
-    <div>
-        <i class="fas fa-street-view"></i>
+    <div
+        title='Show Person Positions'
+        hotkey={hotkeysProxy.layerPersons}
+        use:hotkey
+        use:tooltip
+        on:click={test}
+    >
+        <i class="far fa-street-view event-none"></i>
     </div>
 </nav>
 
