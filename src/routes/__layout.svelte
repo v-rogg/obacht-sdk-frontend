b673a29a179f0017e760bb503f6d36d210aeee48
v-rogg <mail@valentinrogg.de>
2021-07-03 18:50:54 +0200

    Simple html layout fix

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -35,7 +35,13 @@
 <style lang="sass" global>
     @import "../style/theme.sass"
 
+    html, main
+        width: 100%
+        height: 100%
+
     body
+        width: 100%
+        height: 100%
         margin: 0
         padding: 0
         font-family: "Obacht! Plex Sans", sans-serif
